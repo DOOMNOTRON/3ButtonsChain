734234df7d795b35cd96942613f518e62dd2dfc0
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-10-20 15:38:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/a3buttonschain/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a3buttonschain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
